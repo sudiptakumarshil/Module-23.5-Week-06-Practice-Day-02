7ff9de4797eefe37d6d886b3cca687358bc37f31
Sudipto Kumar Shil <sudiptoshil@outlook.com>
2024-09-02 21:28:12 +0600

    Final Commit

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -148,10 +148,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.office365.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = env("EMAIL")
-# EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
-# DEFAULT_FROM_EMAIL = env("EMAIL")
-
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = ""
